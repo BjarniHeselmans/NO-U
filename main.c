7f3e00eded8058d4a5451b1a96484d1fe1c04fcb
Bjarni Heselmans <12302152@student.pxl.be>
2024-05-15 14:00:53 +0200

    added ipextracting in my code

--- main.c
+++ main.c
@@ -35,62 +35,53 @@
 	void OSInit( void ) {}
 	void OSCleanup( void ) {}
 #endif
-
+#define debug
 int initialization();
 int connection( int internet_socket );
 void execution( int internet_socket );
 void cleanup( int internet_socket, int client_internet_socket );
-
+char ip_lookup[30];
 int main( int argc, char * argv[] )
 {
 	//////////////////
 	//Initialization//
 	//////////////////
-
 	OSInit();
-
 	int internet_socket = initialization();
-
+	printf("init\n");
 	//////////////
 	//Connection//
 	//////////////
-
+	printf("starting internet socket\n");
 	int client_internet_socket = connection( internet_socket );
-
+	
 	/////////////
 	//Execution//
 	/////////////
-
 	execution( client_internet_socket );
-
-
 	////////////
 	//Clean up//
 	////////////
-
 	cleanup( internet_socket, client_internet_socket );
-
 	OSCleanup();
-
 	return 0;
 }
-
 int initialization()
 {
 	//Step 1.1
+	
 	struct addrinfo internet_address_setup;
 	struct addrinfo * internet_address_result;
 	memset( &internet_address_setup, 0, sizeof internet_address_setup );
 	internet_address_setup.ai_family = AF_UNSPEC;
 	internet_address_setup.ai_socktype = SOCK_STREAM;
 	internet_address_setup.ai_flags = AI_PASSIVE;
-	int getaddrinfo_return = getaddrinfo( NULL, "22", &internet_address_setup, &internet_address_result );
+	int getaddrinfo_return = getaddrinfo( "::1", "22", &internet_address_setup, &internet_address_result );
 	if( getaddrinfo_return != 0 )
 	{
 		fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
 		exit( 1 );
 	}
-
 	int internet_socket = -1;
 	struct addrinfo * internet_address_result_iterator = internet_address_result;
 	while( internet_address_result_iterator != NULL )
@@ -127,18 +118,14 @@ int initialization()
 		}
 		internet_address_result_iterator = internet_address_result_iterator->ai_next;
 	}
-
 	freeaddrinfo( internet_address_result );
-
 	if( internet_socket == -1 )
 	{
 		fprintf( stderr, "socket: no valid socket address found\n" );
 		exit( 2 );
 	}
-
 	return internet_socket;
 }
-
 int connection( int internet_socket )
 {
 	//Step 2.1
@@ -150,23 +137,84 @@ int connection( int internet_socket )
 		perror( "accept" );
 		close( internet_socket );
 		exit( 3 );
-	}
-	return client_socket;
+	} 
+    
+        // Extracting the IP address
+			char client_ip[INET6_ADDRSTRLEN]; // This can accommodate both IPv4 and IPv6 addresses
+			if (client_internet_address.ss_family == AF_INET)
+			{
+				struct sockaddr_in *s = (struct sockaddr_in *)&client_internet_address;
+				inet_ntop(AF_INET, &(s->sin_addr), client_ip, sizeof client_ip);
+			}
+			else if (client_internet_address.ss_family == AF_INET6)
+			{
+				struct sockaddr_in6 *s = (struct sockaddr_in6 *)&client_internet_address;
+				inet_ntop(AF_INET6, &(s->sin6_addr), client_ip, sizeof client_ip);
+			}
+			else
+			{
+				fprintf(stderr, "Unknown address family\n");
+				close(client_socket);
+				close(internet_socket);
+				exit(4);
+			}
+			printf("connected, ip: %s\n", client_ip);
+			#ifndef debug
+			strcpy(ip_lookup, client_ip);
+			#endif
+			#ifdef debug
+			strcpy(ip_lookup, "94.110.92.242");
+			#endif
+
+			char CLI_buffer[1000];
+			snprintf(CLI_buffer, sizeof(CLI_buffer),"curl http://ip-api.com/json/%s?fields=221", ip_lookup);
+
+			FILE *fp;
+    		char IP_LOG_ITEM[2000];
+
+    		fp = popen(CLI_buffer, "r");
+    		if (fp == NULL) {
+        	printf("Error opening cli buffer\n");
+        	return client_socket;
+    		}
+			
+			fgets(IP_LOG_ITEM, sizeof(IP_LOG_ITEM)-1, fp);
+			printf("%s\n", IP_LOG_ITEM);
+    		pclose(fp);
+
+
+		return client_socket;
 }
 
 void execution( int internet_socket )
 {
-
-	//Step 3.1
+	int number_of_bytes_received = 0;
+	char buffer[2000];
+	
+	
+		number_of_bytes_received = recv( internet_socket, buffer, ( sizeof buffer ) - 1, 0 );
+	if( number_of_bytes_received == -1 )
+	{
+		perror( "recv" );
+	}
+	else
+	{
+		buffer[number_of_bytes_received] = '\0';
+		printf( "Received : %s\n", buffer );
+	}
+	char chartosend[] = "lorum ipsom dolor sit amet\n";
 	int number_of_bytes_send = 0;
-	number_of_bytes_send = send( internet_socket, "Hello TCP world!", 16, 0 );
+	number_of_bytes_send = send( internet_socket, chartosend, strlen(chartosend), 0 );
 	if( number_of_bytes_send == -1 )
 	{
 		perror( "send" );
+
+	}else{
+	printf("sent: %s\n", chartosend);
 	}
 
+	}
 
-}
 
 void cleanup( int internet_socket, int client_internet_socket )
 {
@@ -176,7 +224,6 @@ void cleanup( int internet_socket, int client_internet_socket )
 	{
 		perror( "shutdown" );
 	}
-
 	//Step 4.1
 	close( client_internet_socket );
 	close( internet_socket );
